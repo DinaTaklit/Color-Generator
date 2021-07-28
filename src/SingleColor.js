2c5ba08ced7b0cf44b34e92cdcb8c6200a8173f0
DinaTaklit <ez_taklit@esi.dz>
2021-07-28 02:57:25 +0100

    feat: Singlecolor: Set time out for the alert

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -5,6 +5,15 @@ import rgbToHex from './utils'
 const SingleColor = ({weight, index, hexColor}) => {
     const [alert, setAlert] = useState(false)
     const hexValue = `#${hexColor}`
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setAlert(false)
+        }, 2500)
+        return () => {
+            clearTimeout(timeout)
+        }
+    }, [alert])
+
   return (
     <article
         className={`color ${index > 10 && 'color-light'}`}
@@ -16,7 +25,7 @@ const SingleColor = ({weight, index, hexColor}) => {
     >
         <p className="percent-value">{weight}%</p>
         <p className="color-value">{hexValue}</p>
-        {alert && <p className="alert"> copied to clipboard</p>}
+        {alert && <p className={`alert color ${index > 10 && 'color-light'}`}> copied to clipboard</p>}
     </article>
   )
 }
