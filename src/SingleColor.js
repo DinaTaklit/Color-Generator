a0cc86b1f15aed6bbd10d78633941ca0f99203d3
DinaTaklit <ez_taklit@esi.dz>
2021-07-28 02:45:01 +0100

    feat: add Single Color

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -2,8 +2,19 @@ import { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({weight, index, hexColor}) => {
+    const [alert, setAlert] = useState(false)
+    const hexValue = `#${hexColor}`
+  return (
+    <article
+        className={`color ${index > 10 && 'color-light'}`}
+        style={{backgroundColor:hexValue}}
+    >
+        <p className="percent-value">{weight}%</p>
+        <p className="color-value">{hexValue}</p>
+        {alert && <p className="alert"> copied to clipboard</p>}
+    </article>
+  )
 }
 
 export default SingleColor
